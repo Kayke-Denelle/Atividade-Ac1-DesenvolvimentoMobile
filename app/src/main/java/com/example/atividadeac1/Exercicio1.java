48004ff3d1dc19f2508accb7fae350fd5154e592
Kayke_Denelle <kaykedenelle@gmail.com>
2025-03-17 22:02:47 -0300

    tarefa 1 - finalizada

--- app/src/main/java/com/example/atividadeac1/Exercicio1.java
+++ app/src/main/java/com/example/atividadeac1/Exercicio1.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +23,33 @@ public class Exercicio1 extends AppCompatActivity {
                 Intent intent = new Intent(Exercicio1.this, MainActivity.class);
                 startActivity(intent);
             }
+        });
+
+        EditText editTextNome = findViewById(R.id.editTextNome);
+        EditText editTextIdade = findViewById(R.id.editTextIdade);
+        Button btnVerificar = findViewById(R.id.btnVerificar);
+        TextView textViewResultado = findViewById(R.id.textViewResultado);
+
+        btnVerificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = editTextNome.getText().toString();
+                String idadeTexto = editTextIdade.getText().toString();
+
+                if (nome.isEmpty() || idadeTexto.isEmpty()) {
+                    textViewResultado.setText("Por favor, preencha todos os campos.");
+                    return;
+                }
+
+                int idade = Integer.parseInt(idadeTexto);
+
+                if (idade >= 18) {
+                    textViewResultado.setText(nome + ", você é maior de idade.");
+                } else {
+                    textViewResultado.setText(nome + ", você é menor de idade.");
+                }
+            }
+
         });
     }
 }
